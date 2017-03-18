992d3d2932159b3151922c6445bef1f6c7566486
adelman <samuel.adelman@gmail.com>
2017-03-18 16:02:09 -0400

    slightly increase output nodes, car performs more searching

--- model.py
+++ model.py
@@ -67,7 +67,7 @@ def build_model():
     model.add(Flatten())
     model.add(Dense(1164))
     model.add(Dense(100))
-    model.add(Dense(10))
+    model.add(Dense(14))
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
